258e80ef4b505bbbcc6e1bc1376137bef8377efc
NeuralNat <ifrance@yandex.ru>
2024-02-01 19:21:09 +0300

    HTML/CSS .header-top base .nav-mobile style

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.nav-mobile');
+
+navIcon.addEventListener('click', function () {
+	this.classList.toggle('nav-icon--active');
+	nav.classList.toggle('nav--active');
+});
+
+// Находим ссылки внутри мобильной навигации
+const navLinks = document.querySelectorAll('.nav-mobile a');
+
+// Обходим ссылки методом forEach
+navLinks.forEach(function (item) {
+	// Для каждой ссылки добавляем прослушку по событию "Клик"
+	item.addEventListener('click', function () {
+		navIcon.classList.remove('nav-icon--active'); // Убираем активный класс у иконки моб. навигации
+		nav.classList.remove('.nav-mobile--active'); // Убираем активный класс у блока моб. навигации
+	})
+})
