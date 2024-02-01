112b7ae46ba71d179e08e9c2589e6e0d95a44d0f
NeuralNat <ifrance@yandex.ru>
2024-02-01 22:15:38 +0300

    CSS @media .nav-media correction

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const nav = document.querySelector('.nav-mobile');
 
 navIcon.addEventListener('click', function () {
 	this.classList.toggle('nav-icon--active');
-	nav.classList.toggle('nav--active');
+	nav.classList.toggle('nav-mobile--active');
 });
 
 // Находим ссылки внутри мобильной навигации
