d5c777018fda0c14dade4def2eda65f27f39915e
NeuralNat <ifrance@yandex.ru>
2024-02-03 11:12:00 +0300

    CSS .header-about selector correction

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
     
     const navIcon = document.querySelector('.nav-icon');
     const nav = document.querySelector('.nav-mobile');
+    const bodyEl = document.body;
 
     navIcon.addEventListener('click', function () {
         this.classList.toggle('nav-icon--active');
         nav.classList.toggle('nav-mobile--active');
+        bodyEl.classList.toggle('lock');
     });
 
     // Находим ссылки внутри мобильной навигации
@@ -15,9 +17,9 @@ document.addEventListener('DOMContentLoaded', function(){
     navLinks.forEach(function (item) {
         // Для каждой ссылки добавляем прослушку по событию "Клик"
         item.addEventListener('click', function () {
-            console.log('click')
             navIcon.classList.remove('nav-icon--active'); // Убираем активный класс у иконки моб. навигации
             nav.classList.remove('nav-mobile--active'); // Убираем активный класс у блока моб. навигации
+            bodyEl.classList.remove('lock');
         })
     })
 
